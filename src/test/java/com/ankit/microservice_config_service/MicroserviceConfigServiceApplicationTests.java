83e66f2647957aa6ad3e83c59342564b738b72fd
iamankitkuswaha <ankit21010@iiitd.ac.in>
2024-08-27 14:23:44 +0530

    Config-server configuration

---
+++ src/test/java/com/ankit/microservice_config_service/MicroserviceConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.microservice_config_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
